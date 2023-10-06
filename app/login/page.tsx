0fb2a67ff90d395f3b36a15d3d92624b185c13d4
maryamkhan14 <kmaryam818@gmail.com>
2023-10-06 14:35:04 -0700

    Cleanup login page design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
+import Image from "next/image";
 import Login from "./components/Login";
 async function SignInPage() {
   const session = await getServerSession();
   // if the user is not logged in, show the login page, otherwise redirect to the account page
   if (!session) {
-    return <Login />;
+    return (
+      <section className="relative flex grow ">
+        <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-full lg:py-0">
+          <Login />
+        </div>
+      </section>
+    );
   } else {
     redirect("/");
   }
