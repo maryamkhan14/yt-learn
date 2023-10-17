86f1f9cf7a0ff92a6cc2fdc414fd42ccdfcd09ca
maryamkhan14 <kmaryam818@gmail.com>
2023-10-17 16:37:50 -0700

    Move TimestampParser to lib directory, rename to time

---
+++ lib/time.ts
@@ -0,0 +1,25 @@
+import { TIMESTAMP_REGEX } from "../app/create/[link]/schema";
+
+export function toSeconds(timestamp: string): number {
+  if (TIMESTAMP_REGEX.test(timestamp)) {
+    const times = timestamp.split(":");
+    let seconds = 0;
+    let secondsMultiplier = 3600;
+
+    times.map((time) => {
+      seconds += parseInt(time) * secondsMultiplier;
+      secondsMultiplier /= 60;
+    });
+    return seconds;
+  }
+  return 0;
+}
+export function toTimestamp(seconds: number): string {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor((seconds % 3600) % 60);
+  const hoursString = hours < 10 ? `0${hours}` : `${hours}`;
+  const minutesString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const secondsString = s < 10 ? `0${s}` : `${s}`;
+  return `${hoursString}:${minutesString}:${secondsString}`;
+}
