ae1299815d8258fe0eb0481ecde259a05c501878
maryamkhan14 <kmaryam818@gmail.com>
2023-10-17 16:51:52 -0700

    Move CourseForm, Lessons out of test directory, update type names & creation - Rename form, schema type names using the word "Course" instead of "Parse" - Generate dynamic schemas based on total course duration, so lesson duration does not exceed total course duration

--- app/create/[link]/components/Lessons.tsx
+++ app/create/[link]/components/Lessons.tsx
@@ -1,16 +1,24 @@
 import { useFieldArray, useFormContext } from "react-hook-form";
 import Lesson from "./Lesson";
 import Button from "../../../../components/button/CircularIconOnlyButton";
-function Lessons() {
+function Lessons({ totalLength }: { totalLength: number }) {
   const { control } = useFormContext();
-  const { fields, insert, remove } = useFieldArray({
+  const { fields, insert, remove, move } = useFieldArray({
     control,
     name: "lessons",
   });
+
   return (
     <div className=" flex flex-col items-center gap-7">
-      {fields.map((lesson, idx) => (
-        <Lesson key={lesson.id} idx={idx} insert={insert} remove={remove} />
+      {fields.map((lesson, id) => (
+        <Lesson
+          key={lesson.id}
+          id={id}
+          insert={insert}
+          remove={remove}
+          move={move}
+          maxLength={totalLength}
+        />
       ))}
 
       <Button
