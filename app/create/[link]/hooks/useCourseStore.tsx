1decd2aad52e4642bc50aeaf7dd3ee9bff0b3ce4
maryamkhan14 <kmaryam818@gmail.com>
2023-11-01 14:43:41 -0700

    Remove schema from course store since it is no longer needed

--- app/create/[link]/hooks/useCourseStore.tsx
+++ app/create/[link]/hooks/useCourseStore.tsx
@@ -1,22 +1,18 @@
 import { create } from "zustand";
 import { type Lesson } from "../schema";
-import { type Course } from "../schema";
 import { persist } from "zustand/middleware";
 interface State {
   lessons: Lesson[];
-  schema: Course | null;
   duration: number;
   _hasHydrated: boolean;
 }
 interface Actions {
   updateLessons: (lessons: Lesson[]) => void;
-  updateSchema: (schema: Course) => void;
   updateDuration: (duration: number) => void;
   setHasHydrated: (state: State | boolean) => void;
 }
 const INITIAL_STATE: State = {
   lessons: [],
-  schema: null,
   duration: 0,
   _hasHydrated: false,
 };
@@ -25,9 +21,7 @@ export const useCourseStore = create<State & Actions>()(
   persist(
     (set, _get) => ({
       lessons: INITIAL_STATE.lessons,
-      schema: INITIAL_STATE.schema,
       duration: INITIAL_STATE.duration,
-      updateSchema: (schema: Course) => set((_state) => ({ schema })),
       updateLessons: (lessons: Lesson[]) => set((_state) => ({ lessons })),
       updateDuration: (duration: number) => set((_state) => ({ duration })),
       _hasHydrated: false,
