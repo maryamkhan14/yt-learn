949e456159c413b3e33a7a17070f588bb109e31d
maryamkhan14 <kmaryam818@gmail.com>
2023-11-01 19:10:10 -0700

    Persist timestamp of last course progress save with dayjs

--- app/create/[link]/hooks/useCourseStore.tsx
+++ app/create/[link]/hooks/useCourseStore.tsx
@@ -1,19 +1,23 @@
 import { create } from "zustand";
 import { type Lesson } from "../schema";
 import { persist } from "zustand/middleware";
+import { dayJsInstance as dayjs } from "@/lib/time";
 interface State {
   lessons: Lesson[];
   duration: number;
   _hasHydrated: boolean;
+  lastSaved: string;
 }
 interface Actions {
   updateLessons: (lessons: Lesson[]) => void;
   updateDuration: (duration: number) => void;
+  updateLastSaved: () => void;
   setHasHydrated: (state: State | boolean) => void;
 }
 const INITIAL_STATE: State = {
   lessons: [],
   duration: 0,
+  lastSaved: dayjs().toISOString(),
   _hasHydrated: false,
 };
 
@@ -22,9 +26,12 @@ export const useCourseStore = create<State & Actions>()(
     (set, _get) => ({
       lessons: INITIAL_STATE.lessons,
       duration: INITIAL_STATE.duration,
+      lastSaved: INITIAL_STATE.lastSaved,
+      _hasHydrated: false,
       updateLessons: (lessons: Lesson[]) => set((_state) => ({ lessons })),
       updateDuration: (duration: number) => set((_state) => ({ duration })),
-      _hasHydrated: false,
+      updateLastSaved: () =>
+        set((_state) => ({ lastSaved: dayjs().toISOString() })),
       setHasHydrated: (state) => {
         set({
           _hasHydrated: !!state,
