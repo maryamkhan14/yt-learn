40396853eb27f8207024a7284f5112790849c21b
maryamkhan14 <kmaryam818@gmail.com>
2023-11-17 14:35:59 -0800

    Create auth constants file - Create constants for the 3 different results that NextAuth authentication retrieval can return

---
+++ lib/constants/auth.ts
@@ -0,0 +1,4 @@
+type AuthenticationStatus = "authenticated" | "unauthenticated" | "loading";
+export const AUTHENTICATED: AuthenticationStatus = "authenticated";
+export const UNAUTHENTICATED: AuthenticationStatus = "unauthenticated";
+export const LOADING: AuthenticationStatus = "loading";
