845d0af9e3a83511193262c25a40a419b71c9109
maryamkhan14 <kmaryam818@gmail.com>
2023-11-21 14:32:58 -0800

    Add procedure for creating a new course

--- server/routers/courses.ts
+++ server/routers/courses.ts
@@ -1,7 +1,32 @@
 import { z } from "zod";
-import { getFetch } from "@trpc/client";
 import { createTRPCRouter, publicProcedure } from "@/server/trpc";
+import {
+  CourseSchema,
+  type CourseSchemaType,
+} from "@/app/create/[link]/schema";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/auth";
 export const coursesRouter = createTRPCRouter({
+  post: publicProcedure.input(CourseSchema).mutation(async ({ ctx, input }) => {
+    const { db } = ctx;
+    const session = await getServerSession(authOptions);
+    const course: CourseSchemaType = input;
+    const createdCourse = await db.course.create({
+      data: {
+        duration: course.duration,
+        link: course.link,
+      },
+    });
+    if (!!session) {
+      void db.userCourse.create({
+        data: {
+          courseId: createdCourse.id,
+          creatorId: session.user.id,
+        },
+      });
+    }
+    return createdCourse;
+  }),
   getWithChapters: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
