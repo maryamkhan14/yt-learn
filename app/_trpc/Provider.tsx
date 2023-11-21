b33c5f45a56cc6f40960664141bdd527fc41f9c2
maryamkhan14 <kmaryam818@gmail.com>
2023-11-21 13:55:14 -0800

    Fix TRPC client setup on frontend - Move getBaseUrl to /lib/api module, update TRPC Provider component's imports - Use TRPC Provider component in layout - Remove 'use client' directive in /lib/api file

--- app/_trpc/Provider.tsx
+++ app/_trpc/Provider.tsx
@@ -1,13 +1,8 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useState } from "react";
-import { trpc } from "@/lib/api";
 import { httpBatchLink, loggerLink } from "@trpc/client";
-function getBaseUrl() {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-}
+import { getBaseUrl, trpc } from "@/lib/api";
 export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
