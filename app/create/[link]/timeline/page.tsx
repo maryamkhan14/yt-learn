8f02d688d83bd6ae0a8eb054683028d8a7b62585
maryamkhan14 <kmaryam818@gmail.com>
2023-11-29 14:40:59 -0800

    Replace Timeline custom button with FilledButton component

--- app/create/[link]/timeline/page.tsx
+++ app/create/[link]/timeline/page.tsx
@@ -8,6 +8,7 @@ import { type ZodIssue } from "zod";
 import useLessonIssues from "../hooks/useLessonIssues";
 import HydrationLoader from "@/components/HydrationLoader";
 import Loading from "@/app/loading";
+import FilledButton from "@/components/button/FilledButton";
 function TimelinePage() {
   const { link } = useParams();
   const router = useRouter();
@@ -19,16 +20,13 @@ function TimelinePage() {
   return (
     <>
       <CourseTimeline link={link as string} lessons={lessons} issues={issues} />
-      <button
+
+      <FilledButton
         onClick={() => router.replace(".")}
         role="link"
-        className="group/tl-btn relative m-2  w-1/4 self-center rounded-lg border-2 px-4 py-2 transition-all ease-in hover:px-6"
-      >
-        <div className="absolute inset-0 h-full w-0 rounded-lg bg-red-800 transition-all duration-[250ms]  ease-out group-hover/tl-btn:w-full" />
-        <span className="relative rounded-lg text-white group-hover:text-white">
-          Modify course
-        </span>
-      </button>
+        text="Modify course"
+        buttonStyles="self-center"
+      />
     </>
   );
 }
