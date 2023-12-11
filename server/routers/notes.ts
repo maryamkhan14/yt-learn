46026ba7682b1b6e8a5a5815925e6aa9f7141cfa
maryamkhan14 <kmaryam818@gmail.com>
2023-12-10 16:27:44 -0800

    Create TRPC procedure for retrieving notes by lesson ID

--- server/routers/notes.ts
+++ server/routers/notes.ts
@@ -1,6 +1,39 @@
-import { createTRPCRouter, privateProcedure } from "@/server/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "@/server/trpc";
+import { z } from "zod";
 import { CompletedNoteSchema } from "@/app/course/[courseId]/lesson/[lessonId]/@notes/schema";
+import { type Note } from "@prisma/client";
 export const notesRouter = createTRPCRouter({
+  getByLessonId: publicProcedure
+    .input(z.object({ lessonId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const { lessonId } = input;
+      const { db } = ctx;
+      const notes = await db.note.findMany({
+        where: {
+          lessonId,
+        },
+        orderBy: {
+          time: "asc",
+        },
+      });
+      return notes.reduce(
+        (acc, note) => {
+          const noteTime = ~~note.time;
+          const existingNotes = acc?.[noteTime];
+          return {
+            ...acc,
+            [noteTime]: existingNotes?.length
+              ? ([...existingNotes, note] as Note[])
+              : [note],
+          };
+        },
+        {} as Record<string, Note[]>,
+      );
+    }),
   post: privateProcedure
     .input(CompletedNoteSchema)
     .mutation(async ({ ctx, input }) => {
