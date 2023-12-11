31bf2aeeed89809579d8e4d5a66ff83fafe892d4
maryamkhan14 <kmaryam818@gmail.com>
2023-12-10 16:18:02 -0800

    Move useReactPlayerStore to common hooks folder, track time

--- app/hooks/useReactPlayerStore.ts
+++ app/hooks/useReactPlayerStore.ts
@@ -3,18 +3,23 @@ import type ReactPlayer from "react-player";
 import { type RefObject } from "react";
 interface State {
   playerRef: RefObject<ReactPlayer> | null;
+  time: number;
 }
 interface Actions {
   getCurrentTime: () => number;
   setPlayerRef: (ref: State["playerRef"]) => void;
+  setTime: (time: number) => void;
 }
 const INITIAL_STATE: State = {
   playerRef: null,
+  time: 0,
 };
 
 export const useReactPlayerStore = create<State & Actions>()((set, get) => ({
   playerRef: INITIAL_STATE.playerRef,
+  time: INITIAL_STATE.time,
   getCurrentTime: () => get()?.playerRef?.current?.getCurrentTime() ?? 0,
+  setTime: (time: number) => set((_state) => ({ time })),
   setPlayerRef: (playerRef: State["playerRef"]) =>
     set((_state) => ({
       playerRef,
